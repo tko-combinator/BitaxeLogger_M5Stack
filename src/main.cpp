17900dfbf7491ea15eb675f7b8c54eda64b39698
Daikichi Takoatama <takoyan@pm.me>
2024-10-23 10:24:10 +0900

    Organized documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <M5Unified.h>
-
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
@@ -7,7 +6,6 @@
 #include <time.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
 
 // WiFi接続情報
 char ssid[50];
@@ -17,7 +15,6 @@ char password[100];
 #define JST 3600 * 9
 #define SUMMER_TIME_OFFSET 0
 char NTP_url[100];
-
 
 // InfluxDB接続情報
 char InfluxDB_token[100];
@@ -38,14 +35,15 @@ DeviceAddress sensorAddresses[MAX_SENSORS];
 // SDカード書き込み情報
 #define SDCARD_CHIP_SELECT 4
 
-
-struct TempData {
+struct TempData
+{
     float Q1;
     float Q2;
     float L;
 };
 
-struct BitaxeInfo {
+struct BitaxeInfo
+{
     float power;
     float voltage;
     float current;
@@ -53,27 +51,30 @@ struct BitaxeInfo {
     float hashRate;
 };
 
-
-bool setConfig() {
+bool setConfig()
+{
     File file = SD.open("/config.json", FILE_READ);
 
-    if (!file) {    
-        Serial.println("Failed to open config.json");                                       
+    if (!file)
+    {
+        Serial.println("Failed to open config.json");
         return false;
     }
 
     Serial.println("config.json opened successfully");
 
     String jsonString;
-    while (file.available()) {
+    while (file.available())
+    {
         jsonString += char(file.read());
     }
     file.close();
-    //Serial.println(jsonString);
+    // Serial.println(jsonString);
     DynamicJsonDocument doc(2048);
     DeserializationError error = deserializeJson(doc, jsonString);
 
-    if (error) {
+    if (error)
+    {
         Serial.println("Failed to parse JSON");
         Serial.println(error.c_str());
         return false;
@@ -93,28 +94,29 @@ bool setConfig() {
     return true;
 }
 
-
-void printError(String message) {
-  message = "Error: " + message; 
-  Serial.println(message);
+void printError(String message)
+{
+    message = "Error: " + message;
+    Serial.println(message);
 }
 
-
-void LcdInit() {
-  M5.Display.clear();
-  M5.Display.setTextSize(3);
-  M5.Display.setCursor(10, 10);
-  M5.Lcd.fillScreen(WHITE);
-  M5.Lcd.setTextColor(BLACK, WHITE);
-  M5.Display.println("LCD: OK");
+void LcdInit()
+{
+    M5.Display.clear();
+    M5.Display.setTextSize(3);
+    M5.Display.setCursor(10, 10);
+    M5.Lcd.fillScreen(WHITE);
+    M5.Lcd.setTextColor(BLACK, WHITE);
+    M5.Display.println("LCD: OK");
 }
 
-
-void WiFiInit() {
+void WiFiInit()
+{
     Serial.print("Connecting to WiFi: ");
-    Serial.println(ssid);    
+    Serial.println(ssid);
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(1000);
         Serial.print(".");
     }
@@ -123,32 +125,37 @@ void WiFiInit() {
     Serial.println(WiFi.localIP());
 }
 
-
-void WiFiReconnect() {
-
+void WiFiReconnect()
+{
 }
 
-
-void WiFiError() {
-  printError("WiFi not connected");  
-  WiFiReconnect();
+void WiFiError()
+{
+    printError("WiFi not connected");
+    WiFiReconnect();
 }
 
-void initNTP() {
-    if (WiFi.status() == WL_CONNECTED) {
+void initNTP()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
         configTime(JST, SUMMER_TIME_OFFSET, NTP_url);
         Serial.println("Time synchronized with NTP server.");
-    } else {
+    }
+    else
+    {
         WiFiError();
-    } 
+    }
 }
 
-
-String getLocalTime_NTP() {
+String getLocalTime_NTP()
+{
     tm timeData;
 
-    if (WiFi.status() == WL_CONNECTED) {
-        if (!getLocalTime(&timeData)) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        if (!getLocalTime(&timeData))
+        {
             Serial.println("Failed to obtain time");
             return "00-00-00 00:00:00";
         }
@@ -157,28 +164,34 @@ String getLocalTime_NTP() {
         char timeBuf[20];
         strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", &timeData);
         return String(timeBuf);
-    } else {
+    }
+    else
+    {
         WiFiError();
     }
 }
 
-BitaxeInfo getBitaxeInfo() {
+BitaxeInfo getBitaxeInfo()
+{
     BitaxeInfo info;
 
-    if (WiFi.status() == WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
         HTTPClient http;
         String url = String(Bitaxe_url) + "/system/info";
         http.begin(url);
-        
+
         int httpResponseCode = http.GET();
-        if (httpResponseCode > 0) {
+        if (httpResponseCode > 0)
+        {
             String response = http.getString();
             Serial.println("Bitaxe API Response:");
             Serial.println(response);
             StaticJsonDocument<1024> data;
             DeserializationError error = deserializeJson(data, response);
 
-            if (error) {
+            if (error)
+            {
             }
 
             info.power = data["power"];
@@ -187,74 +200,85 @@ BitaxeInfo getBitaxeInfo() {
             info.temp = data["temp"];
             info.hashRate = data["hashRate"];
 
-            return info;            
-        } else {
+            return info;
+        }
+        else
+        {
             Serial.print("Error on HTTP request: ");
             Serial.println(httpResponseCode);
         }
 
         http.end();
-    } else {
+    }
+    else
+    {
         WiFiError();
     }
 }
 
+void _storeInfluxdb(TempData tempData, BitaxeInfo bitaxeInfo)
+{
+    HTTPClient http;
+    http.setTimeout(5000);
 
-void _storeInfluxdb(TempData tempData,  BitaxeInfo bitaxeInfo) {
-        HTTPClient http;
-        http.setTimeout(5000);
+    String url = String(InfluxDB_url) + "/write" + "?org=" + InfluxDB_org + "&bucket=" + InfluxDB_bucket + "&precision=s";
 
-        String url = String(InfluxDB_url) + "/write" + "?org=" + InfluxDB_org + "&bucket=" + InfluxDB_bucket + "&precision=s";
-        
-        http.begin(url.c_str());
-        http.addHeader("Authorization", String("Token ") + InfluxDB_token);
-        http.addHeader("Content-Type", "text/plain");
-        
-        String payload = "BitaxeLogging,device=bitaxe401Supra001 ";
-        payload += "Q1=" + String(tempData.Q1) + ",";
-        payload += "Q2=" + String(tempData.Q2) + ",";
-        payload += "L=" + String(tempData.L) + ",";
-        payload += "power=" + String(bitaxeInfo.power) + ",";
-        payload += "voltage=" + String(bitaxeInfo.voltage) + ",";
-        payload += "current=" + String(bitaxeInfo.current) + ",";
-        payload += "temp=" + String(bitaxeInfo.temp) + ",";
-        payload += "hashRate=" + String(bitaxeInfo.hashRate);
+    http.begin(url.c_str());
+    http.addHeader("Authorization", String("Token ") + InfluxDB_token);
+    http.addHeader("Content-Type", "text/plain");
 
-        Serial.println("URL for InfluxDB:");
-        Serial.println(url);
+    String payload = "BitaxeLogging,device=bitaxe401Supra001 ";
+    payload += "Q1=" + String(tempData.Q1) + ",";
+    payload += "Q2=" + String(tempData.Q2) + ",";
+    payload += "L=" + String(tempData.L) + ",";
+    payload += "power=" + String(bitaxeInfo.power) + ",";
+    payload += "voltage=" + String(bitaxeInfo.voltage) + ",";
+    payload += "current=" + String(bitaxeInfo.current) + ",";
+    payload += "temp=" + String(bitaxeInfo.temp) + ",";
+    payload += "hashRate=" + String(bitaxeInfo.hashRate);
 
-        Serial.println("Payload for InfluxDB:");
-        Serial.println(payload);
+    Serial.println("URL for InfluxDB:");
+    Serial.println(url);
 
-        int httpResponseCode = http.POST(payload);
+    Serial.println("Payload for InfluxDB:");
+    Serial.println(payload);
 
-        if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println("Response Code: " + String(httpResponseCode));
-            Serial.println("Response Body: " + response);
-        } else {
-            Serial.print("Error on sending POST: ");
-            Serial.println(httpResponseCode);
-        }
+    int httpResponseCode = http.POST(payload);
 
-        http.end();
+    if (httpResponseCode > 0)
+    {
+        String response = http.getString();
+        Serial.println("Response Code: " + String(httpResponseCode));
+        Serial.println("Response Body: " + response);
+    }
+    else
+    {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();
 }
 
-
-void storeDB(TempData tempData, BitaxeInfo bitaxeInfo) {
-    if (WiFi.status() == WL_CONNECTED) {
+void storeDB(TempData tempData, BitaxeInfo bitaxeInfo)
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
         _storeInfluxdb(tempData, bitaxeInfo);
-    } else {
+    }
+    else
+    {
         WiFiError();
     }
 }
 
-
-void storeSDCARD(TempData tempData, BitaxeInfo bitaxeInfo) {
+void storeSDCARD(TempData tempData, BitaxeInfo bitaxeInfo)
+{
     File file = SD.open("/log.csv", FILE_APPEND);
     String localTime = getLocalTime_NTP();
 
-    if (file) {
+    if (file)
+    {
         file.print(localTime);
         file.print(",");
         file.print(bitaxeInfo.power);
@@ -275,35 +299,43 @@ void storeSDCARD(TempData tempData, BitaxeInfo bitaxeInfo) {
         file.println();
         file.close();
         Serial.println("Data written to SD card.");
-    } else {
+    }
+    else
+    {
         Serial.println("Error opening log.csv");
     }
 }
 
-
-void _printAddress_BM18B20(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++) {
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-  Serial.println();
+void _printAddress_BM18B20(DeviceAddress deviceAddress)
+{
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        if (deviceAddress[i] < 16)
+            Serial.print("0");
+        Serial.print(deviceAddress[i], HEX);
+    }
+    Serial.println();
 }
 
-
-int _init_BM18B20() {   
+int _init_BM18B20()
+{
     sensors.begin();
     int deviceCount = sensors.getDeviceCount();
     Serial.print("Found ");
-    Serial.print(deviceCount);  
+    Serial.print(deviceCount);
     Serial.println(" BM18B20 sensors.");
 
-    for (int i = 0; i < deviceCount; i++) {
-        if (sensors.getAddress(sensorAddresses[i], i)) {
+    for (int i = 0; i < deviceCount; i++)
+    {
+        if (sensors.getAddress(sensorAddresses[i], i))
+        {
             Serial.print("Number: ");
             Serial.println(i + 1);
             Serial.print("Address: ");
             _printAddress_BM18B20(sensorAddresses[i]);
-        } else {
+        }
+        else
+        {
             Serial.print("Unable to find address for sensor ");
             Serial.println(i + 1);
         }
@@ -311,11 +343,12 @@ int _init_BM18B20() {
     return deviceCount;
 }
 
-
-float _getTemp_BM18B20(DeviceAddress sensorAddress) {
+float _getTemp_BM18B20(DeviceAddress sensorAddress)
+{
     sensors.requestTemperaturesByAddress(sensorAddress);
     float temp = sensors.getTempC(sensorAddress);
-    if (temp == DEVICE_DISCONNECTED_C) {
+    if (temp == DEVICE_DISCONNECTED_C)
+    {
         Serial.println("Error: Senser falt..");
         return NAN;
     }
@@ -323,7 +356,8 @@ float _getTemp_BM18B20(DeviceAddress sensorAddress) {
     return temp;
 }
 
-TempData getTempData() {
+TempData getTempData()
+{
     TempData data;
     data.Q1 = 25.3;
     data.Q2 = 26.1;
@@ -331,8 +365,8 @@ TempData getTempData() {
     return data;
 }
 
-
-void setup() {
+void setup()
+{
     auto cfg = M5.config();
     M5.begin(cfg);
     SD.begin(SDCARD_CHIP_SELECT);
@@ -348,11 +382,12 @@ void setup() {
     M5.Display.println("NTP: OK");
 }
 
-void loop() {
-  M5.update();
-  BitaxeInfo bitaxeInfo = getBitaxeInfo();
-  TempData tempData = getTempData();
-  storeDB(tempData, bitaxeInfo);
-  storeSDCARD(tempData, bitaxeInfo);
-  delay(30000);
+void loop()
+{
+    M5.update();
+    BitaxeInfo bitaxeInfo = getBitaxeInfo();
+    TempData tempData = getTempData();
+    storeDB(tempData, bitaxeInfo);
+    storeSDCARD(tempData, bitaxeInfo);
+    delay(30000);
 }
